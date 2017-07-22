0676b11b1c8fd2325270ea725837a9d9ba8d7be4
Naoto8734 <n.8734.150@gmail.com>
2017-07-22 12:44:35 +0900

    Initial Commit

---
+++ practice_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice_cpp
+//
+//  Created by Naoto on 2017/07/22.
+//  Copyright © 2017年 NYamashita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
