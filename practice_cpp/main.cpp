7ba3fc333292a3db464d0f325421495c03b17928
Naoto8734 <n.8734.150@gmail.com>
2017-08-14 09:31:20 +0900

    白い立方体が回転するアニメーション。 左クリックで再生、右クリックで停止、ホイールクリックでコマ送り。

--- practice_cpp/main.cpp
+++ practice_cpp/main.cpp
@@ -54,6 +54,7 @@ int edge[][2] = {
  * 関数のプロトタイプ宣言
  *-----------------------------------------------------------------------------------*/
 void Init(void);
+void Idle(void);
 void Display(void);
 void Reshape(int w, int h);
 void Keyboard(unsigned char key, int x, int y);
@@ -70,10 +71,21 @@ void Init(void){
 	glClearColor(0.0, 0.6, 0.0, 0.0);	//黒や白より更新がわかりやすいので、緑でクリア。
 }
 
+
+void Idle(void)
+{
+	glutPostRedisplay();
+}
+
 void Display(void){
-	glClear(GL_COLOR_BUFFER_BIT);
+	static int r = 0;
 	
-	glRotated(25.0, 0.5, 0.5, 0.5);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	
+	gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	glRotated((double)r, 0.1, 1.0, 0.3);
+	
 	glColor3d(1.0, 1.0, 1.0);
 	glBegin(GL_LINES);
 	for (int i = 0; i < 12; ++i) {
@@ -83,25 +95,29 @@ void Display(void){
 	glEnd();
 	
 	glFlush();
+	
+	if (++r >= 360) r = 0;
 }
 
 void Reshape(int w, int h){
 	window_width = w;
 	window_height = h;
 	
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(30.0, (double)h / (double)w, 1.0, 100.0);
-	gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void Keyboard(unsigned char key, int x, int y){
 	//std::cout << key << " key is press at " << x << " , " << y << "\n";
 	switch ( key ){
-		case 0x71:	//q
+		case 'Q':
+		case 'q':
 			Close();
 			break;
 		default:
-			Display();
 			break;
 	}
 }
@@ -111,31 +127,42 @@ void KeyboardUp(unsigned char key, int x, int y){
 }
 
 void Mouse(int button, int state, int x, int y){
-	/*switch (button) {
+	switch (button) {
 		case GLUT_LEFT_BUTTON:
+			if (state == GLUT_DOWN) {
+				glutIdleFunc(Idle);
+			}
 			//std::cout << "left";
 			break;
 		case GLUT_MIDDLE_BUTTON:
+			if (state == GLUT_DOWN){
+				glutPostRedisplay();
+			}
 			//std::cout << "middle";
 			break;
 		case GLUT_RIGHT_BUTTON:
+			if (state == GLUT_DOWN) {
+				glutIdleFunc(0);
+			}
 			//std::cout << "right";
 			break;
 		default:
 			break;
 	}
-	 std::cout << " button is ";
-	 switch (state) {
-		case GLUT_UP:
-	 std::cout << "up";
-	 break;
-		case GLUT_DOWN:
-	 std::cout << "down";
-	 break;
-		default:
-	 break;
-	 }
-	 std::cout << " at " << x <<","<< y << std::endl;*/
+	
+	/*
+	std::cout << " button is ";
+	switch (state) {
+	case GLUT_UP:
+	std::cout << "up";
+	break;
+	case GLUT_DOWN:
+	std::cout << "down";
+	break;
+	default:
+	break;
+	}
+	std::cout << " at " << x <<","<< y << std::endl;*/
 }
 
 
@@ -165,6 +192,7 @@ int main(int argc, char * argv[]) {
 	glutWMCloseFunc(Close);
 	glutKeyboardFunc(Keyboard);
 	glutKeyboardUpFunc(KeyboardUp);
+	glutMouseFunc(Mouse);
 	glutMotionFunc(Motion);
 	
 	glutMainLoop();
