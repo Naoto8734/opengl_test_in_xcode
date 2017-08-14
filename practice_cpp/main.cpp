8331a692b590f8b321d859b6cdd520310387d8a9
Naoto8734 <n.8734.150@gmail.com>
2017-08-14 09:04:27 +0900

    白い立方体が見えるだけ。 何かキーを押すと、立方体が回転する。

--- practice_cpp/main.cpp
+++ practice_cpp/main.cpp
@@ -5,15 +5,14 @@
 //  Created by Naoto on 2017/07/22.
 //  Copyright © 2017年 NYamashita. All rights reserved.
 //
+
 /*-----------------------------------------------------------------------------------*
  * 定数宣言
  *-----------------------------------------------------------------------------------*/
-#define MAXPOINTS 100
 
 /*-----------------------------------------------------------------------------------*
  * インクルード
  *-----------------------------------------------------------------------------------*/
-
 #include <iostream>
 #include <OpenGL/OpenGL.h>
 #include <GLUT/GLUT.h>
@@ -21,12 +20,35 @@
 /*-----------------------------------------------------------------------------------*
  * グローバル変数
  *-----------------------------------------------------------------------------------*/
-int window_width = 320;
-int window_height = 240;
+int window_height = 500;
+int window_width = 500;
 const char GAME_TITLE[] = "Ziggurat";
-GLint point[MAXPOINTS][2];
-int point_num = 0;
-int rubber_band = 0;
+
+GLdouble vertex[][3] = {
+	{ 0.0, 0.0, 0.0 },
+	{ 1.0, 0.0, 0.0 },
+	{ 1.0, 1.0, 0.0 },
+	{ 0.0, 1.0, 0.0 },
+	{ 0.0, 0.0, 1.0 },
+	{ 1.0, 0.0, 1.0 },
+	{ 1.0, 1.0, 1.0 },
+	{ 0.0, 1.0, 1.0 }
+};
+
+int edge[][2] = {
+	{ 0, 1 },
+	{ 1, 2 },
+	{ 2, 3 },
+	{ 3, 0 },
+	{ 4, 5 },
+	{ 5, 6 },
+	{ 6, 7 },
+	{ 7, 4 },
+	{ 0, 4 },
+	{ 1, 5 },
+	{ 2, 6 },
+	{ 3, 7 }
+};
 
 /*-----------------------------------------------------------------------------------*
  * 関数のプロトタイプ宣言
@@ -51,14 +73,14 @@ void Init(void){
 void Display(void){
 	glClear(GL_COLOR_BUFFER_BIT);
 	
-	if (point_num>1) {
-		glColor3f(1.0, 1.0, 1.0);
-		glBegin(GL_LINES);
-		for (int i = 0; i < point_num; i++) {
-			glVertex2iv(point[i]);
-		}
-		glEnd();
+	glRotated(25.0, 0.5, 0.5, 0.5);
+	glColor3d(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+	for (int i = 0; i < 12; ++i) {
+		glVertex3dv(vertex[edge[i][0]]);
+		glVertex3dv(vertex[edge[i][1]]);
 	}
+	glEnd();
 	
 	glFlush();
 }
@@ -67,45 +89,34 @@ void Reshape(int w, int h){
 	window_width = w;
 	window_height = h;
 	
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 	glLoadIdentity();
-	glOrtho(-0.5, (GLdouble)w-0.5, (GLdouble)h-0.5, -0.5, -1.0, 1.0);
+	gluPerspective(30.0, (double)h / (double)w, 1.0, 100.0);
+	gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
 void Keyboard(unsigned char key, int x, int y){
-	std::cout << key << " key is press.\n";
+	//std::cout << key << " key is press at " << x << " , " << y << "\n";
 	switch ( key ){
-		case 'q':
-		case 'Q':
-		case 0x1b:
+		case 0x71:	//q
 			Close();
+			break;
+		default:
+			Display();
 			break;
 	}
 }
 
 void KeyboardUp(unsigned char key, int x, int y){
-	std::cout << key << " key is released.\n";
+	//std::cout << key << " key is released at " << x << " , " << y << "\n";
 }
 
 void Mouse(int button, int state, int x, int y){
-	point[point_num][0] = x;
-	point[point_num][1] = y;
-	switch (button) {
+	/*switch (button) {
 		case GLUT_LEFT_BUTTON:
 			//std::cout << "left";
-			if (state == GLUT_UP) {
-				glColor3d(1.0, 1.0, 1.0);//ボタンを押した位置から線を引く
-				glBegin(GL_LINES);
-				glVertex2iv(point[point_num-1]);
-				glVertex2iv(point[point_num]);
-				glEnd();
-				glFlush();
-			}
-			if (point_num < MAXPOINTS +1) point_num++;
 			break;
 		case GLUT_MIDDLE_BUTTON:
 			//std::cout << "middle";
-			Display();
 			break;
 		case GLUT_RIGHT_BUTTON:
 			//std::cout << "right";
@@ -113,46 +124,23 @@ void Mouse(int button, int state, int x, int y){
 		default:
 			break;
 	}
-	/*
-	std::cout << " button is ";
-	switch (state) {
+	 std::cout << " button is ";
+	 switch (state) {
 		case GLUT_UP:
-			std::cout << "up";
-			break;
+	 std::cout << "up";
+	 break;
 		case GLUT_DOWN:
-			std::cout << "down";
-			break;
+	 std::cout << "down";
+	 break;
 		default:
-			break;
-	}
-	std::cout << " at " << x <<","<< y << std::endl;*/
+	 break;
+	 }
+	 std::cout << " at " << x <<","<< y << std::endl;*/
 }
 
+
 void Motion(int x, int y){
-	static GLint prev_point[2];
-	
-	glEnable(GL_COLOR_LOGIC_OP);
-	glLogicOp(GL_INVERT);
-	
-	glBegin(GL_LINES);
-	if (rubber_band){
-		//以前のラバーバンドの消去
-		glVertex2iv(point[point_num-1]);
-		glVertex2iv(prev_point);
-	}
-	glVertex2iv(point[point_num-1]);
-	glVertex2i(x, y);
-	glEnd();
-	glFlush();
-	
-	glLogicOp(GL_COPY);
-	glDisable(GL_COLOR_LOGIC_OP);
-	
-	prev_point[0] = x;
-	prev_point[1] = y;
-	
-	rubber_band = 1;
-	
+	//std::cout << "Drag : "<< x << " , " << y <<"\n";
 }
 
 void Close(){
@@ -167,7 +155,6 @@ int main(int argc, char * argv[]) {
 	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(window_width, window_height);
 	glutCreateWindow(GAME_TITLE);
 	
@@ -178,7 +165,6 @@ int main(int argc, char * argv[]) {
 	glutWMCloseFunc(Close);
 	glutKeyboardFunc(Keyboard);
 	glutKeyboardUpFunc(KeyboardUp);
-	glutMouseFunc(Mouse);
 	glutMotionFunc(Motion);
 	
 	glutMainLoop();
