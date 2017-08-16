8d424e8b61d76b3fa8e5673bc182115d742695c4
Naoto8734 <n.8734.150@gmail.com>
2017-08-16 11:18:18 +0900

    実験aのひとまずの保存。

--- practice_cpp/main.cpp
+++ practice_cpp/main.cpp
@@ -7,8 +7,19 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <math.h>
 #include <OpenGL/OpenGL.h>
 #include <GLUT/GLUT.h>
+
+static double ex = 0.0, ez = 0.0; /* 視点の位置 */
+static double r = 0.0;            /* 視点の向き */
+static double Vx, Vz, Re = 0.0;
+static double Cd = 0.9;
+static int window_width = 500;
+static int window_height = 500;
+static double t = 0.01;
+static const int TIMER_WAIT = 33;
 
 //プロトタイプ宣言
 void scene(void);
@@ -18,6 +29,9 @@ void close(void);
 void keyboard(unsigned char key, int x, int y);
 void init(void);
 void mouse(int x, int y);
+void timer(int value);
+void click(int button, int state, int x, int y);
+
 
 void scene(void){
 	static GLfloat red[] = { 0.8, 0.2, 0.2, 1.0 };
@@ -76,10 +90,7 @@ void scene(void){
 void display(void)
 {
 	static GLfloat lightpos[] = { 3.0, 4.0, 5.0, 1.0 }; /* 光源の位置 */
-	
-	static double ex = 0.0, ez = 0.0; /* 視点の位置 */
-	static double r = 0.0;            /* 視点の向き */
-	
+
 	/* 画面クリア */
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
@@ -87,6 +98,11 @@ void display(void)
 	glLoadIdentity();
 	
 	/* 視点の移動 */
+	if (r < 0){
+		r += 360.0;
+	}
+	r = fmod(r, 360.0);
+	
 	glRotated(r, 0.0, 1.0, 0.0);
 	glTranslated(ex, 0.0, ez);
 	
@@ -101,6 +117,9 @@ void display(void)
 
 void resize(int w, int h)
 {
+	window_width = w;
+	window_height = h;
+	
 	/* ウィンドウ全体をビューポートにする */
 	glViewport(0, 0, w, h);
 	
@@ -116,15 +135,28 @@ void resize(int w, int h)
 }
 
 void close(void){
+	std::cout << "End game." << std::endl;
 	exit(0);
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
 	/* ESC か q をタイプしたら終了 */
-	if (key == '\033' || key == 'q') {
-		close();
+	switch (key) {
+		case 'q':
+		case 'Q':
+		case 0x1b:
+			close();
+			break;
+		case 0x20:
+			ex = 0;
+			ez = 0;
+			r = 0;
+			break;
+		default:
+			break;
 	}
+	display();
 }
 
 
@@ -139,19 +171,54 @@ void init(void)
 }
 
 void mouse(int x, int y) {
+	double x_d, y_d ,r_d = 0.0;
 	
+	x_d = -(double)(x*2)/(double)window_width + 1.0;
+	y_d = -(double)(y*2)/(double)window_height + 1.0;
+	r_d = -atan(y_d/x_d)*180/M_PI*0.1;
+	
+	Vx = Vx + x_d;
+	Vz = Vz + y_d;
+	Re = Re + r_d;
+
+	std::cout << x_d << " , " << y_d << " , " << r_d << std::endl;
+}
+
+void click(int button, int state, int x, int y){
+	if (button == GLUT_LEFT_BUTTON){
+		if (state == GLUT_DOWN){
+			Cd = 1.0;
+		}else{
+			Cd = 0.9;
+		}
+	}
+}
+
+void timer(int value){
+	glutTimerFunc( TIMER_WAIT, timer, 0);
+	Vx = Vx * Cd;
+	Vz = Vz * Cd;
+	Re = Re * Cd;
+	ex = ex + Vx * t;
+	ez = ez + Vz * t;
+	r = r + Re * t;
+	//std::cout << ex << " , " << ez << " , " << r << std::endl;
+	display();
 }
 
 int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(window_width, window_height);
 	glutCreateWindow(argv[0]);
 	glutDisplayFunc(display);
 	glutReshapeFunc(resize);
 	glutKeyboardFunc(keyboard);
 	glutWMCloseFunc(close);
+	glutMouseFunc(click);
 	glutMotionFunc(mouse);
+	glutTimerFunc( TIMER_WAIT, timer, 0);
 	init();
 	glutMainLoop();
 	return 0;
